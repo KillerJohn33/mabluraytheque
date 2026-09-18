2197fdff55fafa3f1912932037aef9aea47673e7
KillerJohn33 <john33700@gmail.com>
2026-09-18 20:36:06 +0200

    Accelere la detection des codes-barres

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = 'bluraytheque-v6';
+const CACHE_VERSION = 'bluraytheque-v7';
 const APP_SHELL = ['/', '/index.html', '/manifest.webmanifest', '/icons/icon-192.png', '/icons/icon-512.png', '/icons/apple-touch-icon.png'];
 
 self.addEventListener('install', event => {
